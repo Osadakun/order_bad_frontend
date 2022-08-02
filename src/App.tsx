e1fb09cef4a36d93cad0aa95e62a4cf3014c2e50
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-08-02 17:36:01 +0900

    追加したコンポーネントを配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,97 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { createContext, useEffect, useState } from "react";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { getCurrentUser } from "./api/auth";
+import { SignIn } from "./components/pages/auth/SignIn";
+import { SignUp } from "./components/pages/auth/SignUp";
+
 import { Detail } from "./components/pages/post/Detail";
 import { Edit } from "./components/pages/post/Edit";
 import { Home } from "./components/pages/post/Home";
-import { HeaderLayout } from "./components/templates/HeaderLayout";
 import { New } from "./components/pages/post/New";
+import { HeaderLayout } from "./components/templates/HeaderLayout";
 import theme from "./theme/theme";
+import { User } from "./types/user";
+
+export const AuthContext = createContext({});
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
+  const [currentUser, setCurrentUser] = useState<User>();
+
+  const handleGetCurrentUser = async () => {
+    try {
+      const res = await getCurrentUser();
+
+      if (res?.data.isLogin === true) {
+        setIsSignedIn(true);
+        setCurrentUser(res?.data.data);
+        console.log(res.data.data);
+      } else {
+        console.log("no current user");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    handleGetCurrentUser();
+  }, [setCurrentUser]);
+
+  const Private = ({ children }: any) => {
+    if (!loading) {
+      if (isSignedIn) {
+        return children;
+      } else {
+        return <Redirect to="/signin" />;
+      }
+    } else {
+      return <></>;
+    }
+  };
+
   return (
     <ChakraProvider theme={theme}>
-      <BrowserRouter>
-        <Switch>
-          <HeaderLayout>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/new">
-              <New />
-            </Route>
-            <Route path="/post/:id">
-              <Detail />
-            </Route>
-            <Route path="/edit/:id">
-              <Edit />
-            </Route>
-          </HeaderLayout>
-        </Switch>
-      </BrowserRouter>
+      <AuthContext.Provider
+        value={{
+          loading,
+          setLoading,
+          isSignedIn,
+          setIsSignedIn,
+          currentUser,
+          setCurrentUser,
+        }}
+      >
+        <BrowserRouter>
+          <Switch>
+            <HeaderLayout>
+              <Route exact path="/signup">
+                <SignUp />
+              </Route>
+              <Route exact path="/signin">
+                <SignIn />
+              </Route>
+              <Private>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route exact path="/new">
+                  <New />
+                </Route>
+                <Route path="/post/:id">
+                  <Detail />
+                </Route>
+                <Route path="/edit/:id">
+                  <Edit />
+                </Route>
+              </Private>
+            </HeaderLayout>
+          </Switch>
+        </BrowserRouter>
+      </AuthContext.Provider>
     </ChakraProvider>
   );
 }
