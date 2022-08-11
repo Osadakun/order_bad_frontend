8c4ca2f58f6589e6976acd2945c93517d82dfdee
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-08-12 00:11:57 +0900

    オーダーフォームの作成(送信はまだ)

--- src/components/templates/OrderForm.tsx
+++
@@ -1,9 +0,0 @@
-import { Select } from '@chakra-ui/react'
-import { Member } from '../../api/member';
-
-export const OrderForm = (props: Member)  => {
-  return (
-    <Select placeholder='選手名の選択'>
-    </Select>
-  )
-}
