24b9a45b50070b6ed5a78b9963fc0d80aed641a8
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-08-12 00:21:03 +0900

    フォーマットの調整

--- src/components/pages/order/ConfirmOrder.tsx
+++ src/components/pages/order/ConfirmOrder.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Center } from "@chakra-ui/react";
+import { Box, Heading, Center, Stack, Text } from "@chakra-ui/react";
 import { memo, useContext, useEffect, useState, FC } from "react";
 import { getOrder } from "../../../api/order";
 import { AuthContext } from "../../../App";
@@ -24,16 +24,27 @@ export const ConfirmOrder: FC = memo(() => {
   }, []);
 
   return (
-    <Box p="40px">
-      <Heading as="h4" textAlign="center" mb="16px" color="red">
+    <Box>
+      <Center
+        w="80%"
+        h="570px"
+        bg="white"
+        mx="auto"
+        my="10px"
+        borderRadius="md"
+        shadow="md"
+        textAlign="center"
+      >
+        <Stack spacing={4}>
+      <Heading textAlign="center" color="red">
         オーダーの確認
       </Heading>
-      <Heading as="h6" textAlign="center" mb="16px">
+      <Text fontSize="30px" textAlign="center">
         チーム名:{order?.name}
-        <br></br>
+      </Text>
+      <Text fontSize="30px" textAlign="center">
         対戦チーム名:{order?.enemyName}
-      </Heading>
-      <Center w="full" h="200">
+      </Text>
         <Box fontSize="28px">
           第1複：{order?.firstDouble_1}
           <br></br>
@@ -45,6 +56,7 @@ export const ConfirmOrder: FC = memo(() => {
           <br></br>
           第2複：{order?.secondDouble_2}
         </Box>
+        </Stack>
       </Center>
     </Box>
   )
