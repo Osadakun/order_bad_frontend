c6f21f958b3da6fb52c88ebf434d741fd7f0df0f
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-12-14 14:59:10 +0900

    save

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,15 +1,21 @@
-import applyCaseMiddleware from "axios-case-converter";
 import axios from "axios";
 
-const options = {
-  ignoreHeaders: true,
-};
+const getURL = () =>{
+  let url;
+  switch(process.env.NODE_ENV) {
+    case 'production':
+      url = process.env.REACT_APP_API_PROD_URL
+      break
+    case 'development':
+      url = process.env.REACT_APP_API_DEV_URL
+      break
+    default:
+  }
+  return url;
+}
 
-const client = applyCaseMiddleware(
-  axios.create({
-    baseURL: "http://localhost:3001/api/v1",
-  }),
-  options
-);
+const client = axios.create({
+  baseURL: getURL()
+  });
 
 export default client;
