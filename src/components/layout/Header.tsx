b9463b48eb236b0f32f2e246893cd691b5d1344c
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-12-18 01:58:54 +0900

    debug

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -59,7 +59,8 @@ export const Header: FC = memo(() => {
         return (
           <Flex align="center" fontSize="sm">
             <Box mr="24px">
-              チーム名：{currentUser.name}
+              {/* チーム名：{currentUser.name} */}
+              {currentUser}
             </Box>
             <Box>
               <Link onClick={handleSignOut}>ログアウト</Link>
